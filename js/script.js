5f9f5d250522db8de7cc1032892382d0485e1352
karla <karla@getharvest.com>
2021-03-02 15:37:45 -0800

    set default focus

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Set focus to the Name field by default 
+const nameField = document.getElementById('name');
+nameField.focus();
+
+//Hide the Job Role text field unless the Other job option is selected
