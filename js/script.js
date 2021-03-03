7d0c740bf0fd010e147230d9ad9ae5ec1802ecf7
karla <karla@getharvest.com>
2021-03-02 17:03:28 -0800

    toggle shirt color options based on design selection

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
 const nameField = document.getElementById('name');
 const jobRole = document.getElementById('title');
 const jobTextField = document.getElementById('other-job-role');
-const shritColor = document.getElementById('color');
+const shirtDesign = document.getElementById('design');
+const shirtColor = document.getElementById('color');
+const jsPunColors = document.querySelectorAll('[data-theme="js puns"]');
+const heartColors = document.querySelectorAll('[data-theme="heart js"]');
+
 
 //Clear and focus on Name field by default 
 nameField.value = '';
@@ -21,4 +25,30 @@ jobRole.addEventListener ('change', (event) => {
   }
 });
 
-//Disable color menu by default
+//Clear and disable Color menu by default
+shirtColor.value = shirtColor.firstElementChild.value;
+shirtColor.disabled = true;
+//Clear the shirt Design dropdown by default
+shirtDesign.value = shirtDesign.firstElementChild.value;
+//Show t-shirt Color options based on Design selection 
+shirtDesign.addEventListener('change', (event) => {
+  if (event.target.value === 'js puns') {
+    shirtColor.disabled = false;
+    for (let i = 0; i < heartColors.length; i++) {
+      heartColors[i].style.display = 'none';
+    }
+    for (let i = 0; i < jsPunColors.length; i++) {
+      jsPunColors[i].style.display = '';
+    }
+  } else if (event.target.value === 'heart js') {
+    shirtColor.disabled = false; 
+    for (let i = 0; i < jsPunColors.length; i++) {
+      jsPunColors[i].style.display = 'none';
+    }
+    for (let i = 0; i < heartColors.length; i++) {
+      heartColors[i].style.display = '';
+    }
+  } else {
+    shirtColor.disabled = true;
+  }
+});
