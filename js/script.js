128cd10e7649449f35c48f9b839f8f0a631e3f2a
karla <karla@getharvest.com>
2021-03-02 16:30:31 -0800

    toggle other job role text field

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-//Set focus to the Name field by default 
+//Declare variables and select DOM elements from the HTML
 const nameField = document.getElementById('name');
+const jobRole = document.getElementById('title');
+const jobTextField = document.getElementById('other-job-role');
+const shritColor = document.getElementById('color');
+
+//Clear and focus on Name field by default 
+nameField.value = '';
 nameField.focus();
 
+//Clear Job Role and "Other" job text input by default
+jobRole.value = jobRole.firstElementChild.value;
+jobTextField.value = '';
 //Hide the Job Role text field unless the Other job option is selected
+jobTextField.style.display = 'none';
+jobRole.addEventListener ('change', (event) => {
+  if (event.target.value === 'other') {
+    jobTextField.style.display = '';
+  } else {
+    jobTextField.style.display = 'none';
+  }
+});
+
+//Disable color menu by default
