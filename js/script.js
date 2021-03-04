9074ecc2e3bacb39ec0f44f338122192c2b334ab
karla <karla@getharvest.com>
2021-03-03 17:51:29 -0800

    show or hide payment options based on selection

--- js/script.js
+++ js/script.js
@@ -9,11 +9,16 @@ const heartColors = document.querySelectorAll('[data-theme="heart js"]');
 const activities = document.getElementById('activities');
 const checkboxes = document.querySelectorAll('[type=checkbox]');
 const totalCostP = document.getElementById('activities-cost');
+const payment = document.getElementById('payment');
+const creditCard = document.getElementById('credit-card');
+const payPal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
 
 
 //Clear and focus on Name field by default 
 nameField.value = '';
 nameField.focus();
+
 
 //Clear Job Role and "Other" job text input by default
 jobRole.value = jobRole.firstElementChild.value;
@@ -27,6 +32,7 @@ jobRole.addEventListener ('change', (event) => {
     jobTextField.style.display = 'none';
   }
 });
+
 
 //Clear and disable Color menu by default
 shirtColor.disabled = true;
@@ -59,18 +65,16 @@ shirtDesign.addEventListener('change', (event) => {
     showColors(heartColors);
     hideColors(jsPunColors);
     resetColor();
-  } else {
-    shirtColor.disabled = true;
-    resetColor();
   }
 });
 
-//Clear checkboxes and cost amount by default
+
+//Clear activities checkboxes and cost amount by default
 let costAmount = 0;
 for (let i = 0; i < checkboxes.length; i++) {
   checkboxes[i].checked = false;
 }
-//Listen for selected activities checkboxes
+//Listen for selecting/de-selecting activities checkboxes and updating the total cost amount
 activities.addEventListener('change', (event) => {
   let activity = event.target; 
   let activityCost = activity.getAttribute('data-cost');
@@ -81,4 +85,32 @@ activities.addEventListener('change', (event) => {
     costAmount -= +activityCost;
     }
   totalCostP.innerHTML = `$${costAmount}`;
+});
+
+
+//Set cc payment option by default
+function showCreditCard () {
+  payment.value = 'credit-card';
+  creditCard.style.display = '';
+  payPal.style.display = 'none';
+  bitcoin.style.display = 'none';
+}
+showCreditCard();
+//Functions to show or hide payment options
+function showPayment (paymentOption) {
+  if (paymentOption === 'credit-card') {
+    showCreditCard();
+  } else if (paymentOption === 'paypal') {
+    payPal.style.display = '';
+    bitcoin.style.display = 'none';
+    creditCard.style.display = 'none';
+  } else if (paymentOption === 'bitcoin'){
+    bitcoin.style.display = '';
+    payPal.style.display = 'none';
+    creditCard.style.display = 'none';
+  }
+}
+//Event handler for payment options
+payment.addEventListener('change', (event) => {
+  showPayment(event.target.value);
 });
